69872ea004beffe9ad70515cec9a634ac6b7f472
wnsgur1198 <wnsgur1198@naver.com>
2020-06-19 17:53:46 +0900

    first commit

---
+++ Korean_history+TOEIC-reminder/UserControl1.Designer.cs
@@ -0,0 +1,251 @@
+﻿// 소스참조 <http://ojc.asia/bbs/board.php?bo_table=LecCsharpNet&wr_id=53>
+
+using System.Windows.Forms;
+
+namespace Korean_history_TOEIC_reminder
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        /// 
+        
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.toeicLabel = new System.Windows.Forms.Label();
+            this.toeicLink = new System.Windows.Forms.LinkLabel();
+            this.khistoryLabel = new System.Windows.Forms.Label();
+            this.khistoryLink = new System.Windows.Forms.LinkLabel();
+            this.toeicResult = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.khistoryResult = new System.Windows.Forms.DataGridView();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.toeicResult)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.khistoryResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toeicLabel
+            // 
+            this.toeicLabel.AutoSize = true;
+            this.toeicLabel.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.toeicLabel.Location = new System.Drawing.Point(14, 18);
+            this.toeicLabel.Name = "toeicLabel";
+            this.toeicLabel.Size = new System.Drawing.Size(151, 20);
+            this.toeicLabel.TabIndex = 1;
+            this.toeicLabel.Text = "토익 시험 일정";
+            // 
+            // toeicLink
+            // 
+            this.toeicLink.AutoSize = true;
+            this.toeicLink.Location = new System.Drawing.Point(15, 42);
+            this.toeicLink.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.toeicLink.Name = "toeicLink";
+            this.toeicLink.Size = new System.Drawing.Size(65, 15);
+            this.toeicLink.TabIndex = 2;
+            this.toeicLink.TabStop = true;
+            this.toeicLink.Text = "toeicLink";
+            this.toeicLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.toeicLink_LinkClicked);
+            // 
+            // khistoryLabel
+            // 
+            this.khistoryLabel.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.khistoryLabel.Location = new System.Drawing.Point(15, 322);
+            this.khistoryLabel.Name = "khistoryLabel";
+            this.khistoryLabel.Size = new System.Drawing.Size(172, 20);
+            this.khistoryLabel.TabIndex = 1;
+            this.khistoryLabel.Text = "한국사 시험 일정";
+            // 
+            // khistoryLink
+            // 
+            this.khistoryLink.AutoSize = true;
+            this.khistoryLink.Location = new System.Drawing.Point(18, 341);
+            this.khistoryLink.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.khistoryLink.Name = "khistoryLink";
+            this.khistoryLink.Size = new System.Drawing.Size(84, 15);
+            this.khistoryLink.TabIndex = 2;
+            this.khistoryLink.TabStop = true;
+            this.khistoryLink.Text = "khistoryLink";
+            this.khistoryLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.khistoryLink_LinkClicked);
+            // 
+            // toeicResult
+            // 
+            this.toeicResult.AllowUserToAddRows = false;
+            this.toeicResult.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.toeicResult.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.toeicResult.ColumnHeadersHeight = 29;
+            this.toeicResult.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.toeicResult.DefaultCellStyle = dataGridViewCellStyle4;
+            this.toeicResult.Location = new System.Drawing.Point(12, 64);
+            this.toeicResult.Name = "toeicResult";
+            this.toeicResult.ReadOnly = true;
+            this.toeicResult.RowHeadersVisible = false;
+            this.toeicResult.RowHeadersWidth = 51;
+            this.toeicResult.Size = new System.Drawing.Size(796, 242);
+            this.toeicResult.TabIndex = 4;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "컬럼1";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 80;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "컬럼2";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 160;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "컬럼3";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 130;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column4.HeaderText = "컬럼4";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // khistoryResult
+            // 
+            this.khistoryResult.AllowUserToAddRows = false;
+            this.khistoryResult.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.khistoryResult.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.khistoryResult.ColumnHeadersHeight = 29;
+            this.khistoryResult.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8});
+            this.khistoryResult.DefaultCellStyle = dataGridViewCellStyle4;
+            this.khistoryResult.Location = new System.Drawing.Point(12, 368);
+            this.khistoryResult.Name = "khistoryResult";
+            this.khistoryResult.ReadOnly = true;
+            this.khistoryResult.RowHeadersVisible = false;
+            this.khistoryResult.RowHeadersWidth = 51;
+            this.khistoryResult.RowTemplate.Height = 27;
+            this.khistoryResult.Size = new System.Drawing.Size(796, 212);
+            this.khistoryResult.TabIndex = 5;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 125;
+            // 
+            // Column6
+            // 
+            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column6.HeaderText = "Column6";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Column7
+            // 
+            this.Column7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column7.HeaderText = "Column7";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            // 
+            // Column8
+            // 
+            this.Column8.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column8.HeaderText = "Column8";
+            this.Column8.MinimumWidth = 6;
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 611);
+            this.Controls.Add(this.khistoryResult);
+            this.Controls.Add(this.toeicLabel);
+            this.Controls.Add(this.toeicLink);
+            this.Controls.Add(this.toeicResult);
+            this.Controls.Add(this.khistoryLabel);
+            this.Controls.Add(this.khistoryLink);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "UserControl1";
+            this.Text = "한국사 토익 알리미";
+            ((System.ComponentModel.ISupportInitialize)(this.toeicResult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.khistoryResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label toeicLabel;        
+        private System.Windows.Forms.LinkLabel toeicLink;
+        private System.Windows.Forms.DataGridView toeicResult;
+        private System.Windows.Forms.Label khistoryLabel;
+        private System.Windows.Forms.LinkLabel khistoryLink;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private DataGridView khistoryResult;
+        private DataGridViewTextBoxColumn Column5;
+        private DataGridViewTextBoxColumn Column6;
+        private DataGridViewTextBoxColumn Column7;
+        private DataGridViewTextBoxColumn Column8;
+    }
+}
---
+++ Korean_history+TOEIC-reminder/UserControl1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using HtmlAgilityPack;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Korean_history_TOEIC_reminder
+{
+    public partial class UserControl1 : Form
+    {
+        // 진입점에 적합한 정적 'Main'방법을 포함하지 않습니다
+        /* 소스참고 <https://www.it-swarm.dev/ko/c%23/%EC%A7%84%EC%9E%85-%EC%A0%90%EC%9
+        7%90-%EC%A0%81%ED%95%A9%ED%95%9C-%EC%A0%95%EC%A0%81-%27%EB%A9%94%EC%9D%B8%27%EB
+        %B0%A9%EB%B2%95%EC%9D%84-%ED%8F%AC%ED%95%A8%ED%95%98%EC%A7%80-%
+        EC%95%8A%EC%8A%B5%EB%8B%88%EB%8B%A4/942281366/> */
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new UserControl1());
+        }
+
+        string toeiclink = "https://appexam.ybmnet.co.kr/toeic/info/receipt_schedule.asp";
+        string khistorylink = "http://www.historyexam.go.kr/pageLink.do?link=examSchedule";
+
+        public UserControl1()
+        {
+            InitializeComponent();
+
+            // 토익 링크 지정
+            toeicLink.Text = toeiclink;
+
+            // 토익 크롤링 시작
+            crowl_toeic();
+
+            // 한국사시험 링크 지정
+            khistoryLink.Text = khistorylink;
+
+            // 토익 크롤링 시작
+            crowl_khistory();
+        }
+
+        public DataGridView ToeicResult
+        {
+            get { return this.toeicResult; }
+            set { this.toeicResult = value; }
+        }
+
+        public DataGridView kHistoryResult
+        {
+            get { return this.khistoryResult; }
+            set { this.khistoryResult = value; }
+        }
+
+        private void crowl_toeic()
+        {
+            using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+            {
+                AParser c1 = new AParser();
+                c1.ExecuteDemo(toeiclink, ToeicResult);
+            }
+        }
+
+        private void crowl_khistory()
+        {
+            using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+            {
+                AParser c1 = new AParser();
+                c1.ExecuteDemo2(khistorylink, kHistoryResult);
+            }
+        }
+
+        private void toeicLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                VisitLink_toeicLink();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to open link that was clicked.");
+            }
+        }
+
+        private void VisitLink_toeicLink()
+        {
+            toeicLink.LinkVisited = true;
+            System.Diagnostics.Process.Start(toeiclink);
+        }
+
+        private void khistoryLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                VisitLink_khistoryLink();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to open link that was clicked.");
+            }
+        }
+
+        private void VisitLink_khistoryLink()
+        {
+            toeicLink.LinkVisited = true;
+            System.Diagnostics.Process.Start(khistorylink);
+        }
+    }
+
+    // html 한글 인코딩 <http://egloos.zum.com/sjc333/v/3023174>
+    public class AParser
+    {
+        public void ExecuteDemo(string sitesource, DataGridView toeicResult)
+        {
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(sitesource);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //초반 부분에서 charset확인
+            int len = 1024;
+            byte[] barr = new byte[len];
+            Stream stream = response.GetResponseStream();
+            int read_size = stream.Read(barr, 0, (int)len);
+            string data = Encoding.UTF8.GetString(barr);
+            var encod = doc.DetectEncodingHtml(data);
+
+            if (encod == null) encod = Encoding.UTF8;//default
+
+            //나머지 읽기
+            int total_size = read_size;
+            while (read_size > 0)
+            {
+                byte[] barr2 = new byte[total_size * 2];
+                for (int a = 0; a < total_size; a++) barr2[a] = barr[a];//복사
+                read_size = stream.Read(barr, 0, total_size);
+                for (int a = 0; a < read_size; a++) barr2[total_size + a] = barr[a];//복사
+                total_size += read_size;
+                barr = barr2;
+            }
+
+            string convstr = Encoding.Unicode.GetString(Encoding.Convert(encod, Encoding.Unicode, barr));
+            doc.LoadHtml(convstr);
+            response.Close();
+
+            foreach (HtmlNode body in doc.DocumentNode.SelectNodes("//body"))
+            {
+
+                foreach (HtmlNode table in body.SelectNodes("//table[@class = 'table_info_print']"))
+                {
+                    int colCnt = 0;
+                    foreach (HtmlNode th in table.SelectNodes("//th[@class = 'str']"))
+                    {
+                        if (colCnt == 4) break;
+                        toeicResult.Columns[colCnt++].HeaderText = th.InnerText;
+                    }
+
+                    colCnt = 0;
+                    List<string> rowList = new List<string>();
+                    foreach (HtmlNode td in table.SelectNodes("//td[@class = 'str']"))
+                    {
+                        string filtered1 = Regex.Replace(td.InnerText, "[A-Za-z;]", "");
+                        string filtered2 = Regex.Replace(filtered1, "[&]", " ");
+                        filtered2 = filtered2.Replace("\t", "");
+
+                        //Console.WriteLine("Row: " + filtered2 + ", Rows: " + toeicResult.Rows.Count);
+
+                        if (colCnt < 4)
+                        {
+                            rowList.Add(filtered2);
+                            colCnt++;
+
+                            if(colCnt == 4)
+                            {
+                                toeicResult.Rows.Add(rowList.ToArray());
+                                rowList.Clear();
+                                colCnt = 0;
+                            }
+                        }
+
+                    }
+
+                }
+            }
+
+        }
+
+        public void ExecuteDemo2(string sitesource, DataGridView khistoryResult)
+        {
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(sitesource);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //초반 부분에서 charset확인
+            int len = 1024;
+            byte[] barr = new byte[len];
+            Stream stream = response.GetResponseStream();
+            int read_size = stream.Read(barr, 0, (int)len);
+            string data = Encoding.UTF8.GetString(barr);
+            var encod = doc.DetectEncodingHtml(data);
+
+            if (encod == null) encod = Encoding.UTF8;//default
+
+            //나머지 읽기
+            int total_size = read_size;
+            while (read_size > 0)
+            {
+                byte[] barr2 = new byte[total_size * 2];
+                for (int a = 0; a < total_size; a++) barr2[a] = barr[a];//복사
+                read_size = stream.Read(barr, 0, total_size);
+                for (int a = 0; a < read_size; a++) barr2[total_size + a] = barr[a];//복사
+                total_size += read_size;
+                barr = barr2;
+            }
+
+            string convstr = Encoding.Unicode.GetString(Encoding.Convert(encod, Encoding.Unicode, barr));
+            doc.LoadHtml(convstr);
+            response.Close();
+
+            int cnt = 0; int cnt_limit = 20;
+
+            foreach (HtmlNode body in doc.DocumentNode.SelectNodes("//body"))
+            {
+                if (cnt == cnt_limit) break;
+                foreach (HtmlNode table in body.SelectNodes("//table[@summary = '6개등급(1급~6급)']"))
+                {
+                    if (cnt == cnt_limit) break;
+
+                    int colCnt = 0;
+                    foreach (HtmlNode th in table.SelectNodes("//th"))
+                    {
+                        if (colCnt == 4) break;
+                        khistoryResult.Columns[colCnt++].HeaderText = th.InnerText;
+                    }
+
+                    colCnt = 0;
+                    List<string> rowList = new List<string>();
+                    foreach (HtmlNode td in table.SelectNodes("//tbody//td"))
+                    {
+                        if (cnt == cnt_limit) break;
+                        //Console.WriteLine("Row: " + td.InnerText + ", Rows: " + khistoryResult.Rows.Count);
+
+                        if (colCnt < 4)
+                        {
+                            rowList.Add(td.InnerText);
+                            colCnt++;
+
+                            if (colCnt == 4)
+                            {
+                                khistoryResult.Rows.Add(rowList.ToArray());
+                                rowList.Clear();
+                                colCnt = 0;
+                            }
+                        }
+                        cnt++;
+                    }
+                    
+                }
+            }
+
+        }
+
+    }
+
+}
